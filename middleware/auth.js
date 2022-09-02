be7e55a8d31800f779d36a31884c04c8a00d3048
hank <akysn564@gmail.com>
2022-09-02 09:04:43 +0800

    feat(Hank): signinPage UI

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,5 @@
 export default defineNuxtRouteMiddleware(() => {
   // set the redirect code to 301 Moved Permanently
-  const counter = useCookie('counter3');
-  console.log('counter: ', counter);
-  console.log('value: ', counter.value);
   if (counter.value !== 1500) {
     console.log('running');
     return navigateTo('/app/auth/signin');
--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -7,6 +7,24 @@ export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     components,
     directives,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        my: {
+          colors: {
+            background: '#2196F3',
+            surface: '#2196F3',
+            primary: "#00bcd4",
+            secondary: "#8bc34a",
+            accent: "#3f51b5",
+            error: "#e91e63",
+            warning: "#ffeb3b",
+            info: "#2196f3",
+            success: "#4caf50",
+          },
+        },
+      },
+    },
   });
 
   nuxtApp.vueApp.use(vuetify);
